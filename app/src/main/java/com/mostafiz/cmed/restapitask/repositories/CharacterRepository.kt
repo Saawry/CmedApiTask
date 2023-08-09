340d28a672deba800adc7f884524d1bf92a8bf05
Mostafizur Rahman <saawry.hb@gmail.com>
2023-08-10 00:48:24 +0600

    All Character Repository Created

---
+++ app/src/main/java/com/mostafiz/cmed/restapitask/repositories/CharacterRepository.kt
@@ -0,0 +1,24 @@
+package com.mostafiz.cmed.restapitask.repositories
+
+
+
+import com.mostafiz.cmed.restapitask.remote.ApiService
+import com.mostafiz.cmed.restapitask.remote.NetworkResult
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+import javax.inject.Inject
+
+class CharacterRepository @Inject constructor(private val apiService: ApiService) {
+
+
+    suspend fun getAllCharacters()= flow {
+        emit(NetworkResult.Loading(true))
+        val response=apiService.getAllCharacters()
+        emit(NetworkResult.Success(response))
+    }.catch{e->
+        emit(NetworkResult.Failure( e.message?: "Unknown Error"))
+    }
+
+
+}
