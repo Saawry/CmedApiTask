b34a576af5e34e0519c679ac22199e9c0a9006d2
Mostafizur Rahman <saawry.hb@gmail.com>
2023-08-10 00:53:04 +0600

    All Character ViewModel Created

---
+++ app/src/main/java/com/mostafiz/cmed/restapitask/viewmodels/CharacterViewModel.kt
@@ -0,0 +1,33 @@
+package com.mostafiz.cmed.restapitask.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mostafiz.cmed.restapitask.model.CharacterResponseModel
+import com.mostafiz.cmed.restapitask.remote.NetworkResult
+import com.mostafiz.cmed.restapitask.repositories.CharacterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CharacterViewModel @Inject constructor(private val repository: CharacterRepository):ViewModel() {
+
+
+    private var _allCharacterResponse=MutableLiveData<NetworkResult<CharacterResponseModel>>()
+    val allCharacterResponse: LiveData<NetworkResult<CharacterResponseModel>> =_allCharacterResponse
+
+
+
+    fun getAllCharacters(){
+        viewModelScope.launch {
+            repository.getAllCharacters().collect{
+                _allCharacterResponse.postValue(it)
+            }
+        }
+    }
+
+
+
+}
